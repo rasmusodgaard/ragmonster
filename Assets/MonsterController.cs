c03d08723728b2012412ea2bf77c6cf3b92fffc1
rasmusodgaard <r.odgaard@gmail.com>
2021-01-25 23:56:32 +0100

    First commit, simple prototype

---
+++ Assets/MonsterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private Rigidbody2D rb_front;
+
+    public float force = 1;
+
+    private void Awake()
+    {
+        print("Awake?");
+        rb_front = GameObject.FindWithTag("Body_Front").GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rb_front.AddForce((Vector2.right + Vector2.up) * force, ForceMode2D.Impulse);
+            print("Force");
+        }
+    }
+}
